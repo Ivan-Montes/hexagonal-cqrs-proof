fd27dcc13485bc70a0d09b1ede63ce9806ba5d9d
IvanM <ivan.montes@outlook.com>
2024-04-27 08:35:22 +0200

    feat: initial commit ms-artist microservice

---
+++ ms-artist/src/main/java/dev/ime/MsArtistApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsArtistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsArtistApplication.class, args);
+	}
+
+}
