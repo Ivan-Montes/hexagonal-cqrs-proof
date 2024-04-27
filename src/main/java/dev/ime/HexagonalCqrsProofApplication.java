ed83ecd8b759d47f155b2d8dfdcd21315d46e32e
IvanM <ivan.montes@outlook.com>
2024-04-27 08:23:20 +0200

    feat: initial commit

---
+++ src/main/java/dev/ime/HexagonalCqrsProofApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalCqrsProofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalCqrsProofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ime/HexagonalCqrsProofApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalCqrsProofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
