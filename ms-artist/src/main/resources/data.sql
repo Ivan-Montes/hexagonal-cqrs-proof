355752ba5b084ee3fc35a65a1e5185dfc404f4fa
IvanM <ivan.montes@outlook.com>
2024-04-28 10:11:02 +0200

    chore: add initial data for SQL DB

---
+++ ms-artist/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO artists(artist_name, artist_surname, artistic_name) VALUES
+('Barbara', 'Guillén', 'Babi'),
+('Ana', 'García', 'Gata Cattana'),
+('Adam', 'Richard', 'Calvin Harris');
+
+
