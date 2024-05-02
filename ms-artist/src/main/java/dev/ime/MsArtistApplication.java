dc40bc6394cfe3c66232482c26fb83efc7a29e53
IvanM <ivan.montes@outlook.com>
2024-05-02 08:12:44 +0200

    feat: add setting files

--- ms-artist/src/main/java/dev/ime/MsArtistApplication.java
+++ ms-artist/src/main/java/dev/ime/MsArtistApplication.java
@@ -1,13 +1,30 @@
 package dev.ime;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+
+@OpenAPIDefinition (info =
+@Info(
+          title = "API ms-artist",
+          version = "1.0",
+          description = "API ms-artist from hexagonal-cqrs-proof project",
+          license = @License(name = "GNU GPLv3", url = "https://choosealicense.com/licenses/gpl-3.0/"),
+          contact = @Contact(url = "https://github.com/Ivan-Montes", name = "IvanM", email = "ivan@github.com")
+  )
+)
 @SpringBootApplication
 public class MsArtistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsArtistApplication.class, args);
 	}
-
+	
 }
--- ms-artist/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO artists(artist_name, artist_surname, artistic_name) VALUES
-('Barbara', 'Guillén', 'Babi'),
-('Ana', 'García', 'Gata Cattana'),
-('Adam', 'Richard', 'Calvin Harris');
-
-
