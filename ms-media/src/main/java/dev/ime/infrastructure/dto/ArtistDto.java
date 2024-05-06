efc4f53057ec3cd4a5b35e5818e86715c3cdc751
IvanM <ivan.montes@outlook.com>
2024-05-06 08:13:59 +0200

    feat: add dto for Artist and Media classes

---
+++ ms-media/src/main/java/dev/ime/infrastructure/dto/ArtistDto.java
@@ -0,0 +1,20 @@
+package dev.ime.infrastructure.dto;
+
+
+import dev.ime.infrastructure.config.InfrastructureConstant;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+
+public record ArtistDto(
+		Long artistId,
+		@NotEmpty @Size( min = 1, max = 50 ) String artistName,
+		@NotEmpty @Size( min = 1, max = 50 ) String artistSurname,
+		@NotEmpty @Size( min = 1, max = 50 ) String artisticName)
+		{
+	
+	public ArtistDto() {
+		this(0L, InfrastructureConstant.NODATA, "", "");
+	}
+	
+}
