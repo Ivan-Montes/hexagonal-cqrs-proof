5982b919bf126b97c06e315301155eb70f7fce1c
IvanM <ivan.montes@outlook.com>
2024-06-06 08:10:29 +0200

    feat: add validation

--- ms-artist/src/main/java/dev/ime/infrastructure/dto/MediaDto.java
+++ ms-artist/src/main/java/dev/ime/infrastructure/dto/MediaDto.java
@@ -1,15 +1,15 @@
 package dev.ime.infrastructure.dto;
 
 import dev.ime.application.config.ApplicationConstant;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record MediaDto(
 		Long mediaId,
-		@NotEmpty @Size( min = 1, max = 100 ) String mediaName,
-		@NotEmpty @Size( min = 1, max = 50 ) String mediaGenre,
-		@NotEmpty @Size( min = 1, max = 50 ) String mediaClass,
+		@NotBlank @Size( min = 1, max = 100 ) String mediaName,
+		@NotBlank @Size( min = 1, max = 50 ) String mediaGenre,
+		@NotBlank @Size( min = 1, max = 50 ) String mediaClass,
 		@NotNull Long artistId
 		) {
 
